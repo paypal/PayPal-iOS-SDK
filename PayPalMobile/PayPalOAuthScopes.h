d828fed555f50cd4fdde06982c4b76d3353b3f54
Romain Pouclet <romain.pouclet@gmail.com>
2015-04-13 14:21:37 -0400

    Update PayPalOAuthScopes.h
    Add `extern` so constants aren't re-declared every time the `PayPalMobile.h` file is imported.

--- PayPalMobile/PayPalOAuthScopes.h
+++ PayPalMobile/PayPalOAuthScopes.h
@@ -11,17 +11,17 @@
 // @see https://developer.paypal.com/docs/integration/direct/identity/attributes/ for more details
 
 /// Authorize charges for future purchases paid for with PayPal.
-NSString *const kPayPalOAuth2ScopeFuturePayments;
+extern NSString *const kPayPalOAuth2ScopeFuturePayments;
 /// Share basic account information.
-NSString *const kPayPalOAuth2ScopeProfile;
+extern NSString *const kPayPalOAuth2ScopeProfile;
 /// Basic Authentication.
-NSString *const kPayPalOAuth2ScopeOpenId;
+extern NSString *const kPayPalOAuth2ScopeOpenId;
 /// Share your personal and account information.
-NSString *const kPayPalOAuth2ScopePayPalAttributes;
+extern NSString *const kPayPalOAuth2ScopePayPalAttributes;
 /// Share your email address.
-NSString *const kPayPalOAuth2ScopeEmail;
+extern NSString *const kPayPalOAuth2ScopeEmail;
 /// Share your account address.
-NSString *const kPayPalOAuth2ScopeAddress;
+extern NSString *const kPayPalOAuth2ScopeAddress;
 /// Share your phone number.
-NSString *const kPayPalOAuth2ScopePhone;
+extern NSString *const kPayPalOAuth2ScopePhone;
 
